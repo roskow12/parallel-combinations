77fb015001e446855660eef98efecaaa091a63b2
Joe <roskow12@github.com>
2016-12-09 14:28:11 -0800

    no boost, add microbenchmark lib

---
+++ src/main.cpp
@@ -0,0 +1,155 @@
+#include <vector>
+#include <list>
+#include <memory>
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <array>
+#include <chrono>
+#include <future>
+#include <thread>
+#include "parallelCombinations.h"
+
+#include "microbench/microbench.h"
+
+// #define DEBUG_PRINTS
+
+const uint8_t NUM_THREADS = 6;
+
+
+using namespace std::chrono;
+using std::cout;
+using std::endl;
+
+uint64_t acutalNum = 0;
+
+uint8_t largestV(const uint8_t a, const uint8_t b, const uint64_t x) {
+	uint8_t v = a-1;
+
+	while(Combination::numCombinations(v,b) > x)
+		--v;
+
+	return v;
+}
+
+std::vector<uint8_t> mthComb(const uint64_t m, const uint8_t n, const uint8_t k) {
+	std::vector<uint8_t> ans(k, 0);
+
+	uint8_t a = n;
+	uint8_t b = k;
+
+	uint64_t x = (Combination::numCombinations(n, k) - 1) - m;
+
+	for(uint8_t i=0; i<k; ++i) {
+		ans[i] = largestV(a, b, x);
+		x = x - Combination::numCombinations(ans[i], b);
+		a = ans[i];
+		b = b-1;
+	}
+
+	for(uint8_t i=0; i<k; ++i) {
+		ans[i] = (n-1) - ans[i];
+	}
+#ifdef DEBUG_PRINTS
+	std::cout<< m << "th combination: ";
+	for(auto e : ans)
+		cout << unsigned(e) << ' ';
+	cout << '\n';
+#endif
+	return ans;
+}
+
+uint64_t generateCombos3(const int n, const int k) {
+	
+	Combination c(n,k);
+
+	uint64_t total = c.generate();
+
+	if(total != acutalNum)
+		printf("Wrong count computed!\n");	
+
+	return total;
+}
+
+uint64_t generateCombos6(const int n, const int k) {
+	const uint64_t numComb = Combination::numCombinations(n, k);
+
+	const uint64_t chunkSize = (uint64_t)numComb / NUM_THREADS;
+	const uint64_t remainder = numComb % NUM_THREADS;
+
+	std::vector<std::unique_ptr<Combination>> combs;
+	combs.push_back(std::make_unique<Combination>(n, k, chunkSize+1));
+	for (int i = 1; i < NUM_THREADS - 1; ++i)
+		combs.push_back(std::make_unique<Combination>(n, k, chunkSize, mthComb(chunkSize*i, n, k)));
+	combs.push_back(std::make_unique<Combination>(n, k, chunkSize + remainder - 1, mthComb(chunkSize*(NUM_THREADS-1), n, k)));
+
+	std::list<std::future<uint64_t>> futures;
+	for (int i = 0; i < NUM_THREADS; ++i)
+		futures.push_back(std::async(std::launch::async, &Combination::generate, combs.at(i).get()));
+
+	uint64_t total = 0;
+	for (auto it = futures.begin(); it != futures.end(); ++it) {
+		total += (*it).get();
+#ifdef DEBUG_PRINTS		
+		cout << total << '\n';
+#endif
+	}
+	if(total != acutalNum)
+		printf("Wrong count computed!\n");	
+	return total;
+}
+
+const uint64_t ITERATIONS = 1;
+const uint32_t TEST_RUNS = 1;
+
+int main(int argc, const char* argv[]) {
+	cout << "# args: " << argc << '\n';
+
+	const int n = atoi(argv[1]);
+	// const int k = atoi(argv[2]);	
+
+	acutalNum = Combination::numCombinations(n, 8);
+
+	cout << "actual count:\t" << acutalNum << '\n';
+
+	// high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	// cout << "legacy count:\t" << generateCombos5(n, k) << '\n';
+	// high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+	// auto duration0 = duration_cast<microseconds>(t2-t1).count();
+	// cout << "legacy time:\t" << duration0 << '\n';
+
+	// t1 = high_resolution_clock::now();
+	// cout << "new count:\t" << generateCombos7(n, k) << '\n';
+	// t2 = high_resolution_clock::now();
+
+	// auto duration1 = duration_cast<microseconds>(t2-t1).count();
+	// cout << "new time:\t" << duration1 << '\n';
+	
+	// double timeDiff = (((double)duration0/duration1));
+
+	// cout << "new gen was " << std::fixed << std::setprecision(2) << timeDiff << "x faster\n\n";
+
+	moodycamel::stats_t stats = 
+		moodycamel::microbench_stats([&]() {generateCombos3(n,8);}, ITERATIONS, TEST_RUNS);
+	printf("legacy stats: avg: %.3fms, min: %.3fms, max: %.3fms, stddev: %.3fms, median: %.3fms\n",
+	    stats.avg(),
+	    stats.min(),
+	    stats.max(),
+	    stats.stddev(),
+	    stats.median());
+
+	// stats = 
+	// 	moodycamel::microbench_stats([&]() {generateCombos6(n,8);}, ITERATIONS, TEST_RUNS);
+	// printf("new stats: avg: %.3fms, min: %.3fms, max: %.3fms, stddev: %.3fms, median: %.3fms\n",
+	//     stats.avg(),
+	//     stats.min(),
+	//     stats.max(),
+	//     stats.stddev(),
+	//     stats.median());
+
+	// generateCombos5(n,8);
+
+	return 0;
+}
---
+++ src/parallelCombinations.h
@@ -0,0 +1,104 @@
+#ifndef _PARALLEL_COMB_H_
+#define _PARALLEL_COMB_H_
+
+#include <cstdint>
+#include <vector>
+
+class Combination {
+	const uint8_t k;
+	const uint8_t lastIndex;
+	const uint8_t k_minus_2;
+	const uint8_t n_minus_1;
+	const uint8_t n_minus_k;
+	const uint64_t m_combsToCompute;
+	uint64_t count;
+
+public:
+	std::array<uint8_t, 8> currentCombination;
+
+	bool next(){
+		if (++count > m_combsToCompute)
+			return false;
+
+		if (currentCombination[lastIndex] == n_minus_1) {
+			uint8_t i = k_minus_2;
+			while (currentCombination[i] == (n_minus_k+i))
+	            i--;
+
+	        currentCombination[i]++;
+
+	        for(uint8_t j=(i+1); j<k; ++j)
+	        	currentCombination[j] = currentCombination[i]+j-i;
+		}
+		else
+			currentCombination[lastIndex]++;
+
+		return true;
+	}
+
+	uint64_t generate(){
+		while(this->next()) {}		
+		return count-1;
+	}
+
+	Combination(const uint8_t n, const uint8_t k, const uint64_t combsToCompute = 0) :
+		k(k),
+		lastIndex(k-1),
+		k_minus_2(k-2),
+		n_minus_1(n-1),
+		n_minus_k(n-k),
+		m_combsToCompute( (combsToCompute == 0) ? numCombinations(n,k) : combsToCompute),
+		count(0)
+	{
+		for(int i=0; i<k; ++i)
+			currentCombination[i] = i;
+		currentCombination[lastIndex] = k_minus_2;
+	}
+
+	Combination(const uint8_t n, const uint8_t k, const uint64_t combsToCompute,
+		const std::vector<uint8_t> startCombination) : 
+			k(k),
+			lastIndex(k-1),
+			k_minus_2(k-2),
+			n_minus_1(n-1),
+			n_minus_k(n-k),
+			m_combsToCompute(combsToCompute),
+			count(0)
+	{
+		for(int i=0; i<k; ++i)
+			currentCombination[i] = startCombination[i];
+	}
+
+	~Combination() {
+	}
+
+	static uint64_t numCombinations(const long n, const long k) {
+		
+		if(n<k)
+			return 0;
+		else if(n == k)
+			return 1;
+
+		uint64_t delta, iMax;
+
+		const int nMinusK = n-k;
+
+		if(k < nMinusK){
+			delta = nMinusK;
+			iMax = k;
+		}
+		else{
+			delta = k;
+			iMax = nMinusK;
+		}
+
+		uint64_t ans = delta + 1;
+
+		for(uint64_t i=2; i <= iMax; ++i)
+			ans = (ans * (delta+i)) / i;
+
+		return ans;
+	}
+};
+
+#endif //_PARALLEL_COMB_H_
