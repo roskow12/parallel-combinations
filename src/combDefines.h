1e4a52e01f96b383c4756a3cfed16ab7b4db1250
rick <roskow12@github.com>
2016-12-10 13:29:36 -0600

    wip constness

---
+++ src/combDefines.h
@@ -0,0 +1,10 @@
+#include <cstdint> 
+
+constexpr uint8_t N = 100;
+constexpr uint8_t K = 8;
+constexpr uint8_t NUM_THREADS = 4;
+
+constexpr uint8_t LAST_INDEX = K-1;
+constexpr uint8_t K_MINUS_2 = K-2;
+constexpr uint8_t N_MINUS_1 = N-1; 
+constexpr uint8_t N_MINUS_K = N-K; 
--- src/parallelCombinations.h
+++ src/parallelCombinations.h
@@ -5,100 +5,100 @@
 #include <vector>
 
 class Combination {
-	const uint8_t k;
-	const uint8_t lastIndex;
-	const uint8_t k_minus_2;
-	const uint8_t n_minus_1;
-	const uint8_t n_minus_k;
-	const uint64_t m_combsToCompute;
-	uint64_t count;
+  const uint8_t k;
+  const uint8_t lastIndex;
+  const uint8_t k_minus_2;
+  const uint8_t n_minus_1;
+  const uint8_t n_minus_k;
+  const uint64_t m_combsToCompute;
+  uint64_t count;
 
 public:
-	std::array<uint8_t, 8> currentCombination;
+  std::array<uint8_t, 8> currentCombination;
 
-	bool next(){
-		if (++count > m_combsToCompute)
-			return false;
+  bool next() {
+    if (++count > m_combsToCompute)
+      return false;
 
-		if (currentCombination[lastIndex] == n_minus_1) {
-			uint8_t i = k_minus_2;
-			while (currentCombination[i] == (n_minus_k+i))
-	            i--;
+    if (currentCombination[lastIndex] == n_minus_1) {
+      uint8_t i = k_minus_2;
+      while (currentCombination[i] == (n_minus_k + i))
+        i--;
 
-	        currentCombination[i]++;
+      currentCombination[i]++;
 
-	        for(uint8_t j=(i+1); j<k; ++j)
-	        	currentCombination[j] = currentCombination[i]+j-i;
-		}
-		else
-			currentCombination[lastIndex]++;
+      for (uint8_t j = (i + 1); j < k; ++j)
+        currentCombination[j] = currentCombination[i] + j - i;
+    } else
+      currentCombination[lastIndex]++;
 
-		return true;
-	}
+    return true;
+  }
 
-	uint64_t generate(){
-		while(this->next()) {}		
-		return count-1;
-	}
+  uint64_t generate() {
+    while (this->next()) {
+    }
+    return count - 1;
+  }
 
-	Combination(const uint8_t n, const uint8_t k, const uint64_t combsToCompute = 0) :
-		k(k),
-		lastIndex(k-1),
-		k_minus_2(k-2),
-		n_minus_1(n-1),
-		n_minus_k(n-k),
-		m_combsToCompute( (combsToCompute == 0) ? numCombinations(n,k) : combsToCompute),
-		count(0)
-	{
-		for(int i=0; i<k; ++i)
-			currentCombination[i] = i;
-		currentCombination[lastIndex] = k_minus_2;
-	}
+  Combination(const uint8_t n, const uint8_t k,
+              const uint64_t combsToCompute = 0)
+      : k(k), 
+      	lastIndex(k - 1), 	
+      	k_minus_2(k - 2), 	
+      	n_minus_1(n - 1),
+        n_minus_k(n - k),
+        m_combsToCompute((combsToCompute == 0) ? numCombinations(n, k)
+                                               : combsToCompute),
+        count(0) {
+    for (int i = 0; i < k; ++i)
+      currentCombination[i] = i;
+    currentCombination[lastIndex] = k_minus_2;
+  }
 
-	Combination(const uint8_t n, const uint8_t k, const uint64_t combsToCompute,
-		const std::vector<uint8_t> startCombination) : 
-			k(k),
-			lastIndex(k-1),
-			k_minus_2(k-2),
-			n_minus_1(n-1),
-			n_minus_k(n-k),
-			m_combsToCompute(combsToCompute),
-			count(0)
-	{
-		for(int i=0; i<k; ++i)
-			currentCombination[i] = startCombination[i];
-	}
+  Combination(const uint8_t n, const uint8_t k, const uint64_t combsToCompute,
+              const std::vector<uint8_t> startCombination)
+      : k(k), 
+      	lastIndex(k - 1), 
+  		k_minus_2(k - 2), 
+  		n_minus_1(n - 1),
+        n_minus_k(n - k), 
+        m_combsToCompute(combsToCompute), 
+        count(0) {
+    for (int i = 0; i < k; ++i)
+      currentCombination[i] = startCombination[i];
+  }
 
-	~Combination() {
-	}
+  ~Combination() {}
 
-	static uint64_t numCombinations(const long n, const long k) {
-		
-		if(n<k)
-			return 0;
-		else if(n == k)
-			return 1;
+  static uint64_t numCombinations(const long n, const long k) {
 
-		uint64_t delta, iMax;
+    if (n < k)
+      return 0;
+    else if (n == k)
+      return 1;
 
-		const int nMinusK = n-k;
+    uint64_t delta, iMax;
 
-		if(k < nMinusK){
-			delta = nMinusK;
-			iMax = k;
-		}
-		else{
-			delta = k;
-			iMax = nMinusK;
-		}
+    const int nMinusK = n - k;
 
-		uint64_t ans = delta + 1;
+    if (k < nMinusK) {
+      delta = nMinusK;
+      iMax = k;
+    } else {
+      delta = k;
+      iMax = nMinusK;
+    }
 
-		for(uint64_t i=2; i <= iMax; ++i)
-			ans = (ans * (delta+i)) / i;
+    uint64_t ans = delta + 1;
 
-		return ans;
-	}
+    for (uint64_t i = 2; i <= iMax; ++i)
+      ans = (ans * (delta + i)) / i;
+
+    return ans;
+  }
 };
+
+
 
 #endif //_PARALLEL_COMB_H_
